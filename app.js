950a1fc78ed6ce59005e1c1bb4f5b783791cd085
Jacob Flores <jgflores1029@gmail.com>
2026-02-09 00:09:53 -0500

    Add routes and middleware

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
+require('express-async-errors')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
 
 const express = require('express')
 const app = express()
 
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 //middleware
 app.use(express.json())
@@ -12,6 +17,8 @@ app.use(express.json())
 app.get('/', (req,res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
 
 
 
