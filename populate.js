97b28ab051211d0005692b2d1bcc6ebea1cc6140
Jacob Flores <jgflores1029@gmail.com>
2026-02-13 00:48:39 -0500

    update populate.js

--- populate.js
+++ populate.js
@@ -12,9 +12,11 @@ const start = async () => {
         await Product.deleteMany()
         await Product.create(jsonProducts)
         console.log('Success');
+        process.exit(0)
         
     } catch (error) {
         console.log(error)
+        process.exit(1)
     }
 }
 
